9655d6364595c4289dc96da00aae2d96f648b10a
graham-mills <graham-mills@users.noreply.github.com>
2021-08-13 13:20:40 +0100

    Adds frontend integration test harness

--- gen_statemachine/main.py
+++ gen_statemachine/main.py
@@ -27,10 +27,15 @@ class Program:
         args = self.parse_args()
         with open(args.input_file, "r") as file:
             try:
+                LOGGER.info("Generating parse tree..")
                 parse_tree = self.parser.parse_puml(file)
                 LOGGER.debug(f"Generated parse tree:\n{parse_tree}")
+
+                LOGGER.info("Generating statemachine model..")
                 statemachine_model = self.model_builder.build(parse_tree)
             except frontend.ParseError as e:
+                LOGGER.exception(e)
+            except Exception as e:
                 LOGGER.exception(e)
 
 
