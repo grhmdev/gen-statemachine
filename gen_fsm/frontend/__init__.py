e5a9ff2bca90adb76335b1cd234b7ba806d7b17f
graham-mills <graham-mills@users.noreply.github.com>
2021-10-23 14:03:59 +0100

    Fix frontend package imports

--- gen_fsm/frontend/__init__.py
+++ gen_fsm/frontend/__init__.py
@@ -1,3 +1,4 @@
-from gen_fsm.frontend.parser import Parser, ParseError, ParseTree, Node
+from gen_fsm.frontend.parser import Parser, ParseError
+from gen_fsm.frontend.parse_tree import ParseTree, Node
 from gen_fsm.frontend.lexer import Lexer
 from gen_fsm.frontend.tokens import Token, TokenType
